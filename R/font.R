71f1e805fe34de9114b0cb8f0489458b176281b1
Yixuan Qiu <yixuan.qiu@cos.name>
2015-03-10 19:17:30 -0400

    fix font loading problem

--- R/font.R
+++ R/font.R
@@ -4,6 +4,10 @@
 # use font.add() to add default font
 .add.default.font = function()
 {
-    showtextdb::load_fonts()
+    fontfile = system.file("fonts", "wqy-microhei.ttc.zip", package = "showtextdb")
+    outdir = tempdir()
+    utils::unzip(fontfile, exdir = outdir)
+    outfile = file.path(outdir, "wqy-microhei.ttc")
+    sysfonts::font.add("wqy-microhei", outfile)
 }
 
