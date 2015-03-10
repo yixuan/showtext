e2b80aa5d1c295424339ce64569000c8f575a517
Yixuan Qiu <yixuan.qiu@cos.name>
2015-03-10 08:54:14 -0400

    use showtextdb::load_fonts() to load fonts

--- R/font.R
+++ R/font.R
@@ -1,27 +1,9 @@
 # environment to store several important variables
-.pkg.env = new.env();
+.pkg.env = new.env()
 
 # use font.add() to add default font
 .add.default.font = function()
 {
-    if(!"sysfonts" %in% loadedNamespaces())
-        stop("package sysfonts is not loaded");
-    
-    if("wqy-microhei" %in% font.families()) return(invisible(NULL));
-    
-    lib.loc = if("showtext" %in% loadedNamespaces())
-                  dirname(getNamespaceInfo("showtext", "path"))
-              else NULL;
-
-    fontfile = system.file("fonts", "wqy-microhei.ttc.xz",
-                           package = "showtext", lib.loc = lib.loc);
-    con = xzfile(fontfile, "rb");
-    reader = readBin(con, "raw", 5177400);
-    close(con);
-    outfile = file.path(tempdir(), "wqy-microhei.ttc");
-    writeBin(reader, outfile);
-    font.add("wqy-microhei", outfile);
-
-    invisible(NULL);
+    showtextdb::load_fonts()
 }
 
