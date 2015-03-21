29342a3751d85de998e3146bfe29afaf42464de7
Yixuan Qiu <yixuan.qiu@cos.name>
2015-03-20 23:36:22 -0400

    remove nseg option from showtext.begin()

--- R/showtext.R
+++ R/showtext.R
@@ -131,13 +131,10 @@ showtext.opts = function(...)
 #' setwd(old);
 #' 
 #' }
-showtext.begin = function(nseg = 10L)
+showtext.begin = function()
 {
     if(dev.cur() == 1) stop("no active graphics device")
-    if(nseg <= 0) stop("nseg must be positive")
-    if(nseg > 100) stop("nseg too large")
-    
-    .pkg.env$.nseg = as.integer(nseg)
+
     .Call("showtextBegin", PACKAGE = "showtext")
     invisible(NULL)
 }
