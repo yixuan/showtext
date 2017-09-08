3ab98d97f04795945fc747da1096a30146600d76
Yixuan Qiu <yixuan.qiu@cos.name>
2017-09-08 15:05:27 -0400

    register routines

---
+++ src/register_routines.c
@@ -0,0 +1,27 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+#include "outline.h"
+
+SEXP showtext_null_pointer();
+SEXP showtext_new_dev_desc();
+SEXP showtext_free_dev_desc(SEXP ext_ptr);
+SEXP showtext_begin();
+SEXP showtext_end();
+
+static R_CallMethodDef call_methods[] = {
+    {"showtext_new_outline_funs",  (DL_FUNC) &showtext_new_outline_funs,  0},
+    {"showtext_free_outline_funs", (DL_FUNC) &showtext_free_outline_funs, 1},
+    {"showtext_null_pointer",      (DL_FUNC) &showtext_null_pointer,      0},
+    {"showtext_new_dev_desc",      (DL_FUNC) &showtext_new_dev_desc,      0},
+    {"showtext_free_dev_desc",     (DL_FUNC) &showtext_free_dev_desc,     1},
+    {"showtext_begin",             (DL_FUNC) &showtext_begin,             0},
+    {"showtext_end",               (DL_FUNC) &showtext_end,               0},
+    {NULL, NULL, 0}
+};
+
+void R_init_showtext(DllInfo* info)
+{
+    R_registerRoutines(info, NULL, call_methods, NULL, NULL);
+    R_useDynamicSymbols(info, FALSE);
+}
