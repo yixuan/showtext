653640c7e6a66743c8c60135ddc3635673e59e2f
Carson <cpsievert1@gmail.com>
2020-08-04 17:13:25 -0500

    Call showtext_begin's C code via recordGraphics so replayPlot() knows about it, fixes #39

--- R/showtext.R
+++ R/showtext.R
@@ -242,9 +242,16 @@ showtext_begin = function()
         .pkg.env$.use_raster = FALSE
         .pkg.env$.dev_units_per_point = 1.0
     }
-
-    .Call("showtext_begin", PACKAGE = "showtext")
+    
+    grDevices::recordGraphics(
+      showtext_begin_(), as.list(.pkg.env), getNamespace("showtext")
+    )
+    
     invisible(NULL)
+}
+
+showtext_begin_ <- function() {
+  .Call("showtext_begin", PACKAGE = "showtext")
 }
 
 #' @rdname showtext_begin
